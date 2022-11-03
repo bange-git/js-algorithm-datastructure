52c84813283e11f907b5fb40b7597f9964e97f8d
bange-git <sebastienbange@gmail.com>
2022-11-03 06:56:01 +0100

    updated the patterns with more example exercise

--- multiplePointer.js
+++ multiplePointer.js
@@ -15,20 +15,62 @@ const sumZero = (arr) => {
   }
 };
 
-const result = sumZero([-3, -2, -1, 0, 4, 7, 4]);
+const result = sumZero([-3, -2, -1, 0, 4, 3, 4]);
 console.log(result);
 
 //q2 Two pointer starting at index o and 1: count unique values in the array
 
 const countUniqueValues = (arr) => {
-    if(arr.length == 0) return 0;
-    let i = 0;
-   for(let j = 1; j < arr.length; j++){
-     if(arr[i] !== arr[j]){
-        i++;
-        arr[i] = arr[j];
-     }
+  if (arr.length == 0) return 0;
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
     }
-    return i + 1;
+  }
+  return i + 1;
+};
+console.log(countUniqueValues([1, 2, 3, 3, 4]));
+
+//Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+
+const averagePair = (arr, num) => {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start < end) {
+    let avr = (arr[start] + arr[end]) / 2;
+    if (avr == num) {
+      return true;
+    } else if (avr > num) {
+      end--;
+    } else {
+      start++;
+    }
+  }
+  return false;
+};
+
+console.log(averagePair([1, 2, 3], 2.5)); // true
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // true
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); // false
+console.log(averagePair([], 4)); // false
+
+//Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+const isSubsequence = (str1, str2) => {
+ let i  = 0;
+ let j = 0;
+ if(!str1) return true;
+ while( j < str2.length){
+    if(str2[j] == str1[i]) i++;
+    if(i === str1.length) return true;
+    j++;
+ }
+ return false
 }
-console.log(countUniqueValues([1,2,3,3,4]));
+console.log(isSubsequence('hello', 'hello world')); // true
+console.log(isSubsequence('sing', 'sting')); // true
+console.log(isSubsequence('abc', 'abracadabra')); // true
+console.log(isSubsequence('abc', 'abc')); // false (order matters)
--- slidingWindow.js
+++ slidingWindow.js
@@ -17,3 +17,25 @@ const maxSubArray = (arr, num) => {
 };
 
 console.log(maxSubArray([1,2,3,4,5,6,7,7,8,9], 3));
+
+//Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+const maxSubarraySum = (arr, num) => {
+ if(num > arr.length) return null;
+ let maxSum = 0;
+ let tempSum = 0;
+ for(let i = 0; i < num; i++){
+    maxSum += arr[i];
+ }
+ tempSum = maxSum;
+ for(let j = num; j < arr.length; j++){
+    tempSum = tempSum - arr[j - num] + arr[j];
+    maxSum = Math.max(maxSum, tempSum);
+ }
+ return maxSum;
+}
+console.log(maxSubarraySum([100,200,300,400], 2)) // 700
+console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4))  // 39 
+console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
+console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)) // 5
+console.log(maxSubarraySum([2,3], 3)) // null
